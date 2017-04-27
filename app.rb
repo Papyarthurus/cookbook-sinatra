f56a944bffbbe7da31f0ff0e71135a143c45f55f
Arthur Renaud <arthurrenaud.pro@gmail.com>
2017-04-27 16:30:35 +0200

    Sinatra Set Up Checked S3J4

--- app.rb
+++ app.rb
@@ -2,11 +2,15 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+
+set :bind, '0.0.0.0'
+
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
 get '/' do
-  'Hello everybody !!!!!!'
+  @usernames = [ 'ssaunier', 'Papillard' ]
+  erb :index
 end
